563310a639aba80f897e42569d230b22e90e9e46
thedub2001 <thedub2001@hotmail.com>
2016-04-09 19:53:23 +0200

    Simple arduino sketch to read stable values

---
+++ CurrentSensor/ReadCurrentToSerial.ino
@@ -0,0 +1,27 @@
+const int analogInPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(analogInPin, INPUT);
+}
+
+void loop() {
+  Serial.println(currentRead());
+  delay(500);
+}
+
+float currentRead() {
+  int temp = 0;
+  int maxAmp = 0;
+
+  for (int i = 1; i < 200; i++) { //200 represents on cyle of 20ms of the AC current (50Hz)
+    temp = analogRead(A0);
+    if (temp > maxAmp) {
+      maxAmp = temp;
+    }
+    delayMicroseconds(100);
+  }
+  //return (0.02354*maxAmp);
+  return (maxAmp);
+}
